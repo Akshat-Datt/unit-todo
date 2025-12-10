ed520b127404aaceac504f90cf07bf8420727227
Akshat-Datt <dattakshat31@gmail.com>
2025-12-10 21:55:27 +0530

    mainActivity updated with mutable list, recycler view adapters and on click listener to add to do tasks in the list

--- app/src/main/java/com/unit/todoapp/MainActivity.kt
+++ app/src/main/java/com/unit/todoapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.unit.todoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,37 +14,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.unit.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TodoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val rvTodo = findViewById<RecyclerView>(R.id.rvTodo)
+        val btnAddItem = findViewById<Button>(R.id.btnAddItem)
+
+        var todoList = mutableListOf<TodoItem>(
+            TodoItem("New Todo", true)
+        )
+
+        val adapter = TodoAdapter(todoList)
+
+        rvTodo.adapter = adapter
+        rvTodo.layoutManager = LinearLayoutManager(this)
+
+        btnAddItem.setOnClickListener {
+            val title = findViewById<EditText>(R.id.etItemText).toString()
+            val todo = TodoItem(title, false)
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size -1)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoAppTheme {
-        Greeting("Android")
     }
 }
